08834a1c01c133932ca39dfd3879cadaed437d0e
Olha Liashenko <olha.s.liashenko@gmail.com>
2020-09-04 17:20:40 +0300

    Add receiving new message from socket. Refactor

--- app.js
+++ app.js
@@ -4,27 +4,22 @@ const bodyParser = require("body-parser");
 const Message = require("./model/message");
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get("/messages", (req, res) => {
     Message.find({}, (err, messages) => {
         if (err) {
             console.error(err.message);
         } else {
-            res.send(messages);
-        }
-    })
-});
-
-app.post("/messages", (req, res) => {
-    const message = new Message(req.body);
-    message.save((err) => {
-        if (err) {
-            res.status(500);
-            res.render('error', { error: err });
-        } else {
-            res.status(200);
+            res.send(JSON.stringify(messages));
         }
     })
 });
@@ -34,5 +29,26 @@ connectDB();
 const server = app.listen(3000, () => {
     console.log("Hola");
 });
+
+var io = require('socket.io').listen(server);
+
+io.on("connection", (socket) => {
+    console.log("User is connected");
+    socket.on("message", (message) => {
+        console.log("message received:");
+        console.log(message);
+        io.emit("message", message);
+        const messageToSave = new Message({
+            name: message.name,
+            text: message.text
+        });
+        messageToSave.save((err) => {
+            if (err) {
+                res.status(500);
+                res.render('error', { error: err });
+            }
+        })
+    })    
+})
 
 app.use(express.static(__dirname));
--- model/message.js
+++ model/message.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
     name: String,
-    message: String
+    text: String
 });
 
 module.exports = Message = mongoose.model("message", MessageSchema);
